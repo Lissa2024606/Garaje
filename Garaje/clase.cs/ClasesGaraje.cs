5d0322401e01afbf3ec0c32b93d8d3a02dfaf3f9
Maryi corea <maryicorea3@gmail.com>
2025-08-25 21:30:02 -0600

    Agregar archivos de proyecto.

---
+++ Garaje/clase.cs/ClasesGaraje.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garaje.clase.cs
+{
+    internal class ClasesGaraje
+    {
+        private int numeroGaraje;
+        private string descripcion;
+        private int capacidadEspacios;
+        private int espaciosOcupados;
+        public ClasesGaraje(int numeroGaraje, string descripcion, int capacidadEspacios, int espaciosOcupados)
+        {
+            this.numeroGaraje = numeroGaraje;
+            this.descripcion = descripcion;
+            this.capacidadEspacios = capacidadEspacios;
+            this.espaciosOcupados = espaciosOcupados;
+        }
+        // Método para retornar el número de espacios disponibles
+        public int obtenerEspaciosDisponibles()
+        {
+            return capacidadEspacios - espaciosOcupados;
+        }   
+
+
+    }
+    
+    
